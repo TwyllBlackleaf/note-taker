96929888f8b6e5863eb6d296045fd81526a189e1
Twyll <twyllblackleaf@gmail.com>
2021-07-03 17:33:51 -0400

    get routes for index and /notes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const notes = require("./db/db.json");
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,6 +9,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+})
 
 app.listen(PORT, () => {
     console.log(`Server active on port ${PORT}`);
