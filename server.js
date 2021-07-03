7d0811673e1b1ebecb5541217130784281d46a3f
Twyll <twyllblackleaf@gmail.com>
2021-07-03 17:09:49 -0400

    set up express server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server active on port ${PORT}`);
+});
