fe8e80b16947b9cf59bc7f4ce970a4c94bae06e8
Twyll <twyllblackleaf@gmail.com>
2021-07-03 22:52:55 -0400

    GET routes, but buggy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const notes = require("./db/db.json");
+const notes = require(path.join(__dirname, "/db/db.json"));
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,11 +12,15 @@ app.use(express.json());
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
-})
+});
+
+app.get("/api/notes", (req, res) => {
+    res.json(notes);
+});
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server active on port ${PORT}`);
