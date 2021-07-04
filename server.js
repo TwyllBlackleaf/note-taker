291a4eeb8d6770002dab9afd4d1a14febffba42c
Twyll <twyllblackleaf@gmail.com>
2021-07-03 23:30:46 -0400

    started POST route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const notes = require(path.join(__dirname, "/db/db.json"));
+const { nanoid } = require("nanoid");
 
 const PORT = process.env.PORT || 3001;
 
@@ -19,9 +20,17 @@ app.get("/api/notes", (req, res) => {
     res.json(notes);
 });
 
+app.post("/api/notes", (req, res) => {
+    req.body.id = nanoid(7);
+
+    res.json(req.body);
+})
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server active on port ${PORT}`);
