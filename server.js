e13613a717884a600c10ccc7aabcc7ef2c12ef61
Twyll <twyllblackleaf@gmail.com>
2021-07-03 23:42:16 -0400

    validation of new note

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+function validateNote(note) {
+    if (!note.title || typeof note.title !== "string") {
+        return false;
+    }
+    if (!note.text || typeof note.title !== "string") {
+        return false;
+    }
+    return true;
+}
+
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
@@ -23,14 +33,18 @@ app.get("/api/notes", (req, res) => {
 app.post("/api/notes", (req, res) => {
     req.body.id = nanoid(7);
 
-    res.json(req.body);
+    if (!validateNote(req.body)) {
+        res.status(400).send("Please include a title and text in your note.");
+    } else {
+        res.json(req.body);
+    }
+
+
 })
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-
-
 
 app.listen(PORT, () => {
     console.log(`Server active on port ${PORT}`);
